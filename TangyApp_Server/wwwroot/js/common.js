d3eb78967a087c255a4dffada39f08a082484e0c
Kerem Görkem Görgülü <gorkemgorgulu003@gmail.com>
2022-03-08 22:39:02 +0300

    Using Toastr and Calling Javascript in Blazor

---
+++ TangyApp_Server/wwwroot/js/common.js
@@ -0,0 +1,8 @@
+﻿ShowToastr = (command) => {
+    if (command === "success") {
+        toastr.success('Operation Successful', 'Success!', { timeOut: 5000 });
+    }
+    else if (command === "error") {
+        toastr.error('Operation Successfully Failed!', 'Error!', { timeOut: 5000 });
+    }
+}
