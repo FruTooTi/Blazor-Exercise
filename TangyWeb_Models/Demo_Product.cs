351a2278d6ef0a09a3a240dc5ab7e15d48a0f060
Kerem Görkem Görgülü <gorkemgorgulu003@gmail.com>
2022-03-08 18:08:44 +0300

    Initial commit

---
+++ TangyWeb_Models/Demo_Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TangyWeb_Models
+{
+    public class Demo_Product
+    {
+        public int Id { get; set; }
+        public double price { get; set; }
+        public string? Name { get; set; }
+        public bool IsActive { get; set; }
+
+        public string Active = "Active";
+        public string Inactive = "Inactive";
+        public List<Demo_ProductProp> ProductProperties { get; set; }
+    }
+}
---
+++ TangyWeb_Models/Family_Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TangyWeb_Models
+{
+    public class Family_Member
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
